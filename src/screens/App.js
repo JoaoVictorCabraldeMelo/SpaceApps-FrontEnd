9845da7a4fcfebd396741071aabb93194078bc6c
João Victor Cabral de Melo <joao.victor.cabralmelo@gmail.com>
2019-10-19 06:23:56 -0300

    :truck: Criando estrutura dos arquivos
    Src

---
+++ src/screens/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f5f5',
+  },
+});
+
+export default App;
